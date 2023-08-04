638463f6c69cca33e2da6c0c74dc8cac92ca01ec
Lagon <teixgarcia@gmail.com>
2023-08-04 10:48:45 +0200

    Añadir juegos completados mediante un fecth al JSON

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import './styles.css'; // Enlazar los estilos desde el archivo
 
 const achievementsData = [
@@ -8,14 +9,23 @@ const achievementsData = [
   {id: 3,title: 'Primer lugar en el torneo',description: 'Gané el primer lugar en el torneo de League of Legends con mi equipo.',imageUrl: 'https://www.vandalimg.com/psn/plata.png',},
 ];
 
-// Datos de los juegos completados con sus iconos
-const completedGamesData = [
-    { id: 1, title: 'League of Legends', iconUrl: 'https://stardewvalleywiki.com/mediawiki/images/6/68/Main_Logo.png' },
-    { id: 2, title: 'Minecraft', iconUrl: 'https://example.com/minecraft-icon.png' },
-    // Agrega más juegos completados con sus respectivos iconos aquí
-  ];
-
 const HomePage = () => {
+  const [expandedGameId, setExpandedGameId] = useState(null);
+
+  const handleGameClick = (gameId) => {
+    setExpandedGameId((prevExpandedId) => (prevExpandedId === gameId ? null : gameId));
+  };
+
+  const [gamesData, setGamesData] = useState([]);
+
+  useEffect(() => {
+    // Cargar los datos desde gamesData.json usando fetch
+    fetch('/gamesData.json')
+      .then((response) => response.json())
+      .then((data) => setGamesData(data))
+      .catch((error) => console.error('Error al cargar los datos:', error));
+  }, []);
+
   return (
     <div className="page-background">
       <img src="/LOGO2.png" alt="Logo de Logros de Lagon" className="logo" />
@@ -31,23 +41,32 @@ const HomePage = () => {
           </div>
         ))}
       </div>
-    {/* Nueva sección con la segunda card */}
-    <div className="card">
+     {/* Nueva sección con la segunda card */}
+     <div className="card">
         <h1>Juegos Completados</h1>
-        <table>
-
-          <tbody>
-            {completedGamesData.map((game) => (
-              <tr key={game.id}>
-                                <td>
-                  <img src={game.iconUrl} alt={game.title} width="50" height="50" />
-                </td>
-                <td>{game.title}</td>
-
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div className="gallery">
+          {gamesData.map((game) => (
+            <div key={game.id} className="gallery-item">
+              <div className="game-icon" onClick={() => handleGameClick(game.id)}>
+                <img src={game.iconUrl} alt={game.title} width="50" height="50" />
+              </div>
+              <p>{game.title}</p>
+              {expandedGameId === game.id && (
+                <div className="game-achievements">
+                  {game.achievements.map((achievement) => (
+                    <div key={achievement.id} className="achievement">
+                      <img src={achievement.imageUrl} alt={achievement.title} />
+                      <div>
+                        <h2>{achievement.title}</h2>
+                        <p>{achievement.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
