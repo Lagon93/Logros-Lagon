e48ba095468158c5990488ba115e70b82bc36372
Lagon <teixgarcia@gmail.com>
2023-09-13 10:43:28 +0200

    Agregada pantalla de no logros

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/styles.css'; // Importa los estilos desde el archivo CSS
-import Popup from './Popup';
+import React, { useState, useEffect } from "react";
+import "../styles/styles.css"; // Importa los estilos desde el archivo CSS
+import Popup from "./Popup";
 
 // Define el componente HomePage
 const HomePage = () => {
@@ -10,7 +10,7 @@ const HomePage = () => {
   const [selectedGame, setSelectedGame] = useState(null); // Juego seleccionado
   const [isPopupOpen, setIsPopupOpen] = useState(false); // Estado para abrir/cerrar un popup
   const [completedGamesCount, setCompletedGamesCount] = useState(0); // Cantidad de juegos completados
-
+  var juegosD = false;
   // Función para generar un número aleatorio entre 1 y 3
   const getRandomRocaNumber = () => {
     return Math.floor(Math.random() * 3) + 1;
@@ -32,62 +32,105 @@ const HomePage = () => {
   // Efecto secundario que se ejecuta al cargar el componente
   useEffect(() => {
     // Carga los datos de logros diarios desde un archivo JSON
-    fetch('/achievementsData.json')
+    fetch("/gamediarios.json")
       .then((response) => response.json())
       .then((data) => {
         setAchievementsData(data);
       })
-      .catch((error) => console.error('Error al cargar los datos de logros diarios:', error));
+      .catch((error) =>
+        console.error("Error al cargar los datos de logros diarios:", error)
+      );
 
     // Carga los datos de juegos desde un archivo JSON
-    fetch('/games.json')
-    .then((response) => response.json())
-    .then((data) => {
-      // Filtra los juegos con platinum en true
-      const platinumGames = data.filter((game) => game.platinum === true);
-      setGamesData(platinumGames); // Actualiza gamesData con los juegos filtrados
-      setCompletedGamesCount(platinumGames.length); // Actualiza el recuento solo con los juegos filtrados
-    })
-    .catch((error) => console.error('Error al cargar los datos de juegos de la galería:', error));
-}, []); // El efecto se ejecuta solo una vez, ya que la dependencia es un arreglo vacío
+    fetch("/games.json")
+      .then((response) => response.json())
+      .then((data) => {
+        // Filtra los juegos con platinum en true
+        const platinumGames = data.filter((game) => game.platinum === true);
+        setGamesData(platinumGames); // Actualiza gamesData con los juegos filtrados
+        setCompletedGamesCount(platinumGames.length); // Actualiza el recuento solo con los juegos filtrados
+      })
+      .catch((error) =>
+        console.error(
+          "Error al cargar los datos de juegos de la galería:",
+          error
+        )
+      );
+  }, []); // El efecto se ejecuta solo una vez, ya que la dependencia es un arreglo vacío
+
+  const ComparaFecha = (timestamp) => {
+    let timestampn= Number.parseInt(timestamp);
+    var date = new Date(timestampn * 1000); // Multiplica el timestamp por 1000 para obtener milisegundos
+    let dateh = new Date().toLocaleDateString("en-IN");
+    console.log(date === dateh);
+    console.log(date)
+    return date === dateh;
+
+  };
 
   // Renderiza el contenido del componente
   return (
     <div className="page-background">
-      <div className='logo-background'><img src="/LOGO2.png" alt="Logo de Logros de Lagon" className="logo" /></div>
+      <div className="logo-background">
+        <img src="/LOGO2.png" alt="Logo de Logros de Lagon" className="logo" />
+      </div>
       <div className="card">
-        <h1 id="diarios"><img src="/titulo1.png" alt='Titulo logros diarios'></img></h1><div className='rock-achiev'></div>
-        {/* Mapea los datos de logros diarios y renderiza cada logro */}
-        {achievementsData.map((achievement) => (
-          <div key={achievement.id} className="achievement">
-            <div className="roca-image">
-              <img src={`/rock${getRandomRocaNumber()}.png`} alt="Roca" />
-            </div>
-            <div>
-              <h2>{achievement.title}</h2>
-              <p>{achievement.description}</p>
-            </div>
-          </div>
-        ))}
+        <h1 id="diarios">
+          <img src="/titulo1.png" alt="Titulo logros diarios"></img>
+        </h1>
+        <div className="rock-achiev"></div>
+        <div className="logrosdiarios">
+          {/* Mapea los datos de logros diarios y renderiza cada logro */}
+          {achievementsData.map((game) =>
+            game.achievements.map((achievement) => {
+              if (ComparaFecha(achievement.unlocktime)) {
+                console.log("esto que es");
+                // Renderizar contenido cuando game.completed sea verdadero
+                juegosD = true;
+                return (
+                  <div key={achievement.id} className="achievement">
+                    <div className="roca-image">
+                      <img
+                        src={`/rock${getRandomRocaNumber()}.png`}
+                        alt="Roca"
+                      />
+                    </div>
+                    <img src={achievement.image} alt={achievement.name}></img>
+                    <div>
+                      <h2>{achievement.name}</h2>
+                      <p>{achievement.description}</p>
+                    </div>
+                    <div className="verticalLine">
+                      <p></p>
+                    </div>
+                  </div>
+                );
+              }
+            })
+          )}
+          {juegosD ? null : <img src="/nologros.png" alt="Hoy no hay logros" className="noLogros" />}
+        </div>
       </div>
 
       <div className="card2">
-        <h1 className="completed-heading">JUEGOS COMPLETADOS : {completedGamesCount}</h1>
+        <h1 className="completed-heading">
+          JUEGOS COMPLETADOS : {completedGamesCount}
+        </h1>
         <div className="gallery">
           {/* Filtra los juegos con platinum en true y luego mapea */}
-    {gamesData
-      .filter((game) => game.platinum === true)
-      .map((game) => (
-            <div key={game.gameName} className="gallery-item">
-              <div
-                className="game-icon"
-                onClick={() => handleGameClick(game.id)}
-              >
-                <img src={game.image} alt={game.gameName} />
+          {gamesData
+            .filter((game) => game.platinum === true)
+            .map((game) => (
+              <div key={game.gameName} className="gallery-item">
+                <div
+                  className="game-icon"
+                  onClick={() => handleGameClick(game.id)}
+                >
+                  <img src={game.image} alt={game.gameName} />
+                </div>
+                <p>{game.gameName}</p>
               </div>
-              <p>{game.gameName}</p>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
 
